ca75807306b977433607ea596838c4425dde7ad9
iamminhquan <minhquan542005@gmail.com>
2025-10-09 14:10:11 +0700

    feat: add login form

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,52 +1,74 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using BaiTapNhom02_Lan_02.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace BaiTapNhom02_Lan_02.Controllers;
-
-public class HomeController : Controller
+// Minh Quân.
+// Đưa class HomeController vào namespace BaiTapNhom02_Lan_02.Controllers.
+// Ngày chỉnh sửa: 08/10/2025 - 11:25 PM.
+namespace BaiTapNhom02_Lan_02.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
+    // Minh Quân.
+    // Thay default contructor bằng primary contructor.
+    // Ngày chỉnh sửa: 08/10/2025 - 11:27 PM.
+    public class HomeController(ILogger<HomeController> logger) : Controller
+    {
+        private readonly ILogger<HomeController> _logger = logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+        public IActionResult Index()
+        {
+            ViewBag.isIndex = true;
+            return View();
+        }
 
-    public IActionResult Index()
-    {
-        ViewBag.isIndex = true;
-        return View();
-    }
-    public IActionResult Single()
-    {
-        return View();
-    }
-    public IActionResult Bicycles()
-    {
-        ViewBag.pageName = "Bicycles";
-        return View();
-    }
-    public IActionResult Parts()
-    {
-        ViewBag.pageName = "Parts";
-        return View();
-    }
-    public IActionResult Cart()
-    {
-        return View();
-    }
-    public IActionResult Accessories()
-    {
-        ViewBag.pageName = "Accessories";
-        return View();
-    }
+        public IActionResult Single()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        ViewBag.pageName = "Extras";
-        return View();
+        public IActionResult Bicycles()
+        {
+            ViewBag.pageName = "Bicycles";
+            return View();
+        }
+
+        public IActionResult Parts()
+        {
+            ViewBag.pageName = "Parts";
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Accessories()
+        {
+            ViewBag.pageName = "Accessories";
+            return View();
+        }
+
+        // Minh Quân
+        // Thêm chức năng đăng ký.
+        // Ngày chỉnh sửa: 08/10/2025 - 11:30 PM.
+        public IActionResult SignUp()
+        {
+            ViewBag.pageName = "SignUp";
+            return View();
+        }
+
+        // Minh Quân
+        // Thêm chức năng đăng nhập.
+        // Ngày chỉnh sửa: 08/10/2025 - 11:22 PM.
+        public IActionResult SignIn()
+        {
+            ViewBag.pageName = "SignIn";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            ViewBag.pageName = "Extras";
+            return View();
+        }
     }
 }
-//new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }
--- obj/Debug/net8.0/BaiTapNhom02_Lan_02.AssemblyInfo.cs
+++ obj/Debug/net8.0/BaiTapNhom02_Lan_02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTapNhom02_Lan_02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab31c4e74acc55d8346280b309c502adc7d20dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e504901d2b585bad3f5abfeb087f3183d5ea1e40")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTapNhom02_Lan_02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTapNhom02_Lan_02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
